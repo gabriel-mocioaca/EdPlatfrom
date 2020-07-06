f44d5b3a1b946b0a52b574d0e087a32d155c8662
Gabriel Mocioaca <gabriel.mocioaca@gmail.com>
2020-07-06 21:01:08 +0300

    Classroom model

---
+++ EdPlatform/EdPlatform.ApplicationLogic/Data/Classroom.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EdPlatform.ApplicationLogic.Data
+{
+  public class Classroom
+  {
+    public string ClassroomId { get; set; }
+    public string Name { get; set; }
+
+  }
+}
