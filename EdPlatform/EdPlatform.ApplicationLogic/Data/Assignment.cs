f89457161176aa3359e8399a5b141e322572c268
Gabriel Mocioaca <gabriel.mocioaca@gmail.com>
2020-07-07 08:48:36 +0300

    Assignment model

---
+++ EdPlatform/EdPlatform.ApplicationLogic/Data/Assignment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EdPlatform.ApplicationLogic.Data
+{
+  public class Assignment
+  {
+    public string AssignmentId;
+    public string Title;
+    public string Description;
+    public int Deadline;
+  }
+}
