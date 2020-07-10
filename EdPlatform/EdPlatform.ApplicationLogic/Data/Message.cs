06ecf87db076cc9fdc31bddf702a4badb461276c
Gabriel Mocioaca <gabriel.mocioaca@gmail.com>
2020-07-10 18:54:50 +0300

    Message class

---
+++ EdPlatform/EdPlatform.ApplicationLogic/Data/Message.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EdPlatform.ApplicationLogic.Data
+{
+  public class Message
+  {
+    public string MessageId { get; set; }
+    public string Text { get; set; }
+  }
+}
