06ba88cf4b31aea9696871666893095b8f532a42
Gabriel Mocioaca <gabriel.mocioaca@gmail.com>
2020-07-11 06:48:18 +0300

    More migrations

---
+++ EdPlatform/EdPlatform.EFDataAccess/Migrations/20200710211058_mig3.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EdPlatform.EFDataAccess.Migrations
+{
+    public partial class mig3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CreatorName",
+                table: "Classrooms",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatorName",
+                table: "Classrooms");
+        }
+    }
+}
