be8cafb7c5a1d833c4504d4d96f550d5af2991f8
Gabriel Mocioaca <gabriel.mocioaca@gmail.com>
2020-07-11 06:48:49 +0300

    Student and Classroom students placeholders

---
+++ EdPlatform/EdPlatform.ApplicationLogic/Data/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EdPlatform.ApplicationLogic.Data
+{
+  public class Student
+  {
+    public string StudentId { get; set; }
+    public string Name { get; set; }
+    public string Group { get; set; }
+
+    public ICollection<ClassroomStudents> ClassroomStudents { get; set; }
+  }
+}
